f7fc9fab153ffa01ea6e871ebbb7c8cae627c7dc
Ajay kumar <ajcoder25@gmail.com>
2024-02-25 07:11:32 +0530

    Learned about the method

--- 01-variables-data-types/10-capitalize-challenge/script.js
+++ 01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,60 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
-const myString = 'developer';
+// # Capitalize Challenge
+
+// **Instructions**:
+
+// Take the variable `myString` and capitalize the first letter of the word using some of the methods that we talked about in the last video. Put the result in a variable called `myNewString`.
+
+// Create multiple solutions if you would like.
+
+// **Expected Result:**
+
+// ```JavaScript
+// const myString = 'developer';
+
+// console.log(myNewString); // 'Developer'
+
+
+
+//solution
+
+/*const myString = 'developer';
+
+
+let myNewString;
+
+myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
+
+
+console.log(myNewString)
+
+*/
+
+
+//2nd way 
+
+
+/*let myString = 'developer';
+
+let myNewString;
+
+myNewString = myString[0].toUpperCase() + myString.slice(1);
+
+console.log(myNewString);
+
+*/
+
+
+//3rd Way-- solve it by template literal
+
+let myString = 'developer';
+
+let myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`
+
+console.log(myNewString);
+
+
+
+
+
+
---
+++ 01-variables-data-types/11-numbers/script.js
@@ -0,0 +1,31 @@
+
+
+const num = new Number(5.)
+
+
+
+// Number 
+
+// let x = 5 
+
+console.log(typeof x) // Number 
+
+// whereas if you create a number using constructor it will show the object 
+
+
+let y = new Number(5.456);
+
+
+console.log(typeof y) // output will be objecct
+
+
+
+
+
+
+
+let x = 5;
+
+x = num.toString();
+
+console.log(x);
