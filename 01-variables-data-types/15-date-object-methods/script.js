8bc7650dbfb019f654b4eae7625ab3e0087b85c1
Ajay kumar <ajcoder25@gmail.com>
2024-02-27 07:05:43 +0530

    Learning Date Method and API

---
+++ 01-variables-data-types/15-date-object-methods/script.js
@@ -0,0 +1,22 @@
+//How to check the dates 
+
+
+let x ; 
+
+let d = new Date('1')
+
+
+x = d.toString(x);
+x = d.toString(x);
+
+
+// if we want to see the timestamp of the date 
+
+x = d.getTime();
+x = d.valueOf();
+
+x = d.getFullYear();
+
+
+
+console.log(x)
