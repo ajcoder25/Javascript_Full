7bec7fa7256cb7c39b1dd67c9d486ac06a9055e1
Ajay kumar <ajcoder25@gmail.com>
2024-03-01 20:45:28 +0530

    revising array and object

--- 02-arrays-and-objects/01-array-basics/script.js
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -1,46 +1,107 @@
-const Name = [24,56,35,64,65]
+// const Name = [24,56,35,64,65]
 
 
 
-console.log(Name);
+// console.log(Name);
 
 
 
 
-//Array constructor
+// //Array constructor
 
 
-const fruit = new Array('Apple', 'Orrange', 'Kiwi', 'Banana')
+// const fruit = new Array('Apple', 'Orrange', 'Kiwi', 'Banana')
 
 
-console.log(fruit);
-
-
-
-// I can also add the Array literal like this 
+// console.log(fruit);
 
 
 
-x = Name[0];
-x = Name[3] + Name[2]
-
-console.log(x);
+// // I can also add the Array literal like this 
 
 
 
-console.log(`My favorite fruit ${fruit[3]}`)
+// x = Name[0];
+// x = Name[3] + Name[2]
+
+// console.log(x);
 
 
-// we can also change the Array Name 
 
-fruit[3] = 'new'
+// console.log(`My favorite fruit ${fruit[3]}`)
 
-x = fruit;
+
+// // we can also change the Array Name 
+
+// fruit[3] = 'new'
+
+// x = fruit;
 
 
 // want to add to the array 
 
 
-fruit[fruit.length]  = 'strawberry'
+// fruit[fruit.length]  = 'strawberry'
+
+// console.log(fruit);
+
+
+//
+
+const fruit = new Array('orange','grape', 'orange')
+
 
 console.log(fruit);
+
+
+
+//push
+
+const add = [100,101, 102,103,104]
+
+add.push(200);
+
+console.log(add);
+
+
+//pop();
+
+const substract= [100,99,98,97,96,95]
+
+substract.pop();
+console.log(substract);
+
+
+//unshift 
+
+const addBegining = [199,200,201,202,203,204,205]
+
+addBegining.unshift(100);
+
+console.log(addBegining);
+
+// shift 
+
+
+addBegining.reverse();
+console.log(addBegining)
+
+
+//include()- 
+
+//  const x  = addBegining.index(205);
+
+// console.log(x);
+
+
+
+// const check = [20,21,22,23,24,25,26]
+
+
+// const x = check.indexOf(26)
+
+// console.log(x);
+
+
+
+let x ;
--- 02-arrays-and-objects/05-object-literals/script.js
+++ 02-arrays-and-objects/05-object-literals/script.js
@@ -1,13 +1,58 @@
-let x;
+// let x;
 
 //creating the object 
 
-const person = {
-     name: 'John Doe',
-     age: 30,
-     isAdmin:true,
-     address:{ street: '123 Main street st'}
-}
+// const person = {
+//      name: 'John Doe',
+//      age: 30,
+//      isAdmin:true,
+//      address:{ street: '123 Main street st'}
+// }
 
 
-// we can 
+// // we can 
+
+
+
+// //creating an object 
+
+
+// const fullInfo = {
+//                    Name: 'Ajay',
+//                    lastName: 'Yadav',
+//                    address:{
+//                     galiNo: 'new Market',
+//                     streetNo: '144'
+
+//                    }
+
+
+
+
+
+
+// }
+
+
+
+// //updating the properties 
+
+// fullInfo.Name ='pooja'
+
+// const x = fullInfo.Name
+
+// console.log(x);
+
+
+//Array of the object 
+
+const toDo = [
+     {task1: 'Milk'},
+     {task2: 'Coding'},
+     {task3:'Breakfast'},
+     {task4:'Codingagain'},
+]
+
+const y = toDo[2]
+
+console.log(y)
