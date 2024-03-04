f2df11d1c909de9af7f4b4c454532f69250ba4ff
Ajay kumar <ajcoder25@gmail.com>
2024-03-04 06:39:07 +0530

    If else statement

--- 03-functions-scope/09-function-challenges/script.js
+++ 03-functions-scope/09-function-challenges/script.js
@@ -1,28 +1,65 @@
 // Question - 1st 
 
-function getCelsius(F){
-   const celsius =  (F - 32) * 5 / 9
+// function getCelsius(F){
+//    const celsius =  (F - 32) * 5 / 9
 
-   return `User wants to convert ${F} to ${celsius.toFixed(2)}\xB0C`
-}
+//    return `User wants to convert ${F} to ${celsius.toFixed(2)}\xB0C`
+// }
 
 
-console.log(getCelsius(100))
+// console.log(getCelsius(100))
 
 
 // Question - 2nd 
 
-const minMax= (number) =>{
+// const minMax= (number) =>{
      
-    const min = Math.min(...number);
-    const max = Math.max(...number);
+//     const min = Math.min(...number);
+//     const max = Math.max(...number);
 
-    return{min, max};
-}
+//     return{min, max};
+// }
 
 
-const checkNumber = [1,2,3,4,5,6,7,8,9]
-const result = minMax(checkNumber);
+// const checkNumber = [1,2,3,4,5,6,7,8,9]
+// const result = minMax(checkNumber);
 
-console.log(result)
+// console.log(result)
+
+
+
+// const minMax = (number) =>{
+//     const min = Math.min(...number);
+//     const max = Math.max(...number);
+//     return {min, max};
+
+// }
+
+// const check = [1,2,3,4,50,100,120]
+// const result = minMax(check)
+// console.log(result);
+
+// // question 3 
+
+// ((length, breath) =>{
+//     const area = length * breath;
+//     const output = `The area of rectangle with the length ${length} and a width of ${breath} is  ${area}`;
+//     console.log(output);
+// })(10,5);
+
+
+//question
+((number) =>{
+    const min = Math.min(...number)
+    const max = Math.max(...number)
+    console.log(`The smallest number is ${min} and highest number is ${max}`)
+    
+    
+})([1,2,3,4,5,6,7]);
+
+
+
+
+
+
 
---
+++ 04-logic-control-flow/03-switches/script.js
@@ -0,0 +1,42 @@
+const d = new  Date (2022,1,10,8,0,0);
+
+const month = d. getMonth();
+const hour = d.getHours();
+
+
+switch(month){
+    case 1: 
+    console.log('It is January');
+    break;
+
+    case 2:
+        console.log('It is february')
+        break;
+
+    case 3:
+        console.log('It is March')
+
+        break;
+
+        default:
+            console.log('it is not jan,feb or March')
+}
+
+
+
+switch(true){
+    case hour < 12:
+        console.log('Good Morning')
+        break;
+
+    case hour < 18:
+
+    console.log('Good Afternoon')
+    
+    break;
+
+    default:
+
+    console.log('Good night')
+
+}
