496f89d7256dd993336c9ca125f4e23525d592eb
Ajay kumar <ajcoder25@gmail.com>
2024-03-12 07:06:40 +0530

    For Each

--- 05-iteration-array-methods/07-forEach/script.js
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -1,7 +1,33 @@
 const social = ['Linkeid', 'Instagram', 'twitter', 'facebook']
 
 
-social.forEach(function(check){
+// social.forEach(function(check){
 
-    console.log(check);
-})
+//     console.log(check);
+// })
+
+
+
+
+
+///For Each 
+
+
+social.forEach((social, index,arr)=> console.log(`${social} - ${index} , ${arr}`));
+
+
+
+//For each for the object 
+
+
+const socialObj = [
+
+    {name:'Twitter', url: 'https://twitter.com'},
+    {name:'Linkeid', url: 'https://linkeid.com'},
+    {name:'instagram',url: 'https://instagram.com'},
+];
+
+
+socialObj.forEach((socialCheck)=> console.log(socialCheck));
+
+
