cad2c575d600949643dbd125d24b34dba22d661f
Ajay kumar <ajcoder25@gmail.com>
2024-03-18 07:22:15 +0530

    Reduce methos

--- 05-iteration-array-methods/09-map/script.js
+++ 05-iteration-array-methods/09-map/script.js
@@ -1,81 +1,115 @@
-const number = [1,2,3,4,5,6]
 
 
-// const doubledNumber = number.map((number) => number * 2);
+
+// // const doubledNumber = number.map((number) => number * 2);
+
+// // console.log(doubledNumber);
+
+
+
+// // const newNumber = [1,2,3,5,6,7]
+
+// // const tripleNumber = newNumber.map((newNumber) =>  newNumber *10)
+
+
+// // console.log(tripleNumber);
+
+
+
+// ///////////////////////////////////////////////////////
+
+
+// // doing the same with forEach
+
+// const doubledNumber = [];
+
+// number.forEach(checkNumber =>{
+//     doubledNumber.push(checkNumber *2);
+// })
+
 
 // console.log(doubledNumber);
 
 
 
-// const newNumber = [1,2,3,5,6,7]
-
-// const tripleNumber = newNumber.map((newNumber) =>  newNumber *10)
 
 
-// console.log(tripleNumber);
-
-
-
-///////////////////////////////////////////////////////
-
-
-// doing the same with forEach
-
-const doubledNumber = [];
-
-number.forEach(checkNumber =>{
-    doubledNumber.push(checkNumber *2);
-})
-
-
-console.log(doubledNumber);
+// const companies = [
+//     { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+//     { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+//     { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+//     { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+//     { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+//     { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+//     { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+//     { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+//     { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
+//   ];
 
 
 
 
+//   // create and array of company names 
 
-const companies = [
-    { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
-    { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
-    { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
-    { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
-    { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
-    { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
-    { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
-    { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
-    { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
-  ];
+//   const checkCompany = companies.map((newCompany) => newCompany.name);
+//   console.log(checkCompany);
 
 
+//   // Create an array with just company and category
 
+//   const companyInfo = companies.map((company) =>{
+//     return{
+//         name:company.name,
+//         category:company.category,
+//     };
+//   });
 
-  // create and array of company names 
-
-  const checkCompany = companies.map((newCompany) => newCompany.name);
-  console.log(checkCompany);
-
-
-  // Create an array with just company and category
-
-  const companyInfo = companies.map((company) =>{
-    return{
-        name:company.name,
-        category:company.category,
-    };
-  });
-
-  console.log(companyInfo);
+//   console.log(companyInfo);
 
 
   // create the array of object with the name and length of each company in year
 
 
-  const infoCompany = companies.map((info) =>{
-    return{
-        name: info.name,
-        length: info.end - info.start + ' years ',
-    };
-  });
+  // const infoCompany = companies.map((info) =>{
+  //   return{
+  //       name: info.name,
+  //       length: info.end - info.start + ' years ',
+  //   };
+  // });
 
 
-  console.log(infoCompany);
+  // console.log(infoCompany);
+
+
+// Chain Method 
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const checkEven = numbers.filter((number) => number %2 ===0);
+
+
+// console.log(checkEven)
+
+
+
+// // find for divisible by 3
+
+// const oddnumber = numbers.filter((odd) => odd % 3 ===0).map((odd) => odd *10)
+
+// console.log(oddnumber);
+
+
+// How to add the 
+
+
+const sum = numbers.reduce(function(accumulator,currentValue){
+  return accumulator + currentValue;
+},0);
+
+
+console.log(sum)
+
+
+
+const newNumber = [1,2,3,4,5]
+
