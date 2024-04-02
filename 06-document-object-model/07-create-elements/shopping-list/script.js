29981dbba631127a9fca957121b49872cd9509b2
Ajay kumar <ajcoder25@gmail.com>
2024-04-02 07:07:15 +0530

    Learn about the methods

--- 06-document-object-model/07-create-elements/shopping-list/script.js
+++ 06-document-object-model/07-create-elements/shopping-list/script.js
@@ -1,35 +1,93 @@
-//Quick & dirty 
+// //Quick & dirty 
 
-// function CreateListItem(item){
+// // function CreateListItem(item){
+// //       const li = document.createElement('li')
+
+// //       li.innerHTML = `${item}<button class="remove-item btn-link text-red">
+// //       <i class="fa-solid fa-xmark"></i>
+// //     </button>`;
+// //     document.querySelector('.items').appendChild(li);
+// // }
+
+
+
+
+
+// // step -1st Creating the list item
+
+// function createListItem(item){
+
 //       const li = document.createElement('li')
 
+//       //Step 2nd  Adding content to the list 
+
 //       li.innerHTML = `${item}<button class="remove-item btn-link text-red">
-//       <i class="fa-solid fa-xmark"></i>
-//     </button>`;
-//     document.querySelector('.items').appendChild(li);
+//              <i class="fa-solid fa-xmark"></i>
+//            </button>`;
+
+// // Step 3
+
+// document.querySelector('.items').appendChild(li)
 // }
 
 
+// //Clean & performant way
+
+
+// function createNewItem(item){
+
+//   // step 1st: to create the list item
+//   const li = document.createElement('li');
+
+//   //step 1.1 
+    
+//   li.appendChild(document.createTextNode(item));
 
 
 
-// step -1st Creating the list item
+//   // step2nd - will create the button 
+   
+//   const button  = document.querySelector('button')
 
-function createListItem(item){
+//   // step 2.1 Now will create the classes
+//   button.className = "remove-item btn-link text-red";
+//   // step 3 we also want icon so we set that 
 
-      const li = document.createElement('li')
+//   const icon = document.createElement('i')
 
-      //Step 2nd  Adding content to the list 
+//   // step 3.1 
 
-      li.innerHTML = `${item}<button class="remove-item btn-link text-red">
-             <i class="fa-solid fa-xmark"></i>
-           </button>`;
+//   icon.className = 'fa-solid fa-xmark'
 
-// Step 3
-
-document.querySelector('.items').appendChild(li)
-}
+//   button.appendChild(icon);
+//   li.appendChild(button);
+//   document.querySelector('.items').appendChild(li);
 
 
-createListItem('Egg')
-createNew
+
+
+// }
+
+// createListItem('Egg')
+// createNewItem('cheese')
+
+
+
+
+
+// step 1 
+
+const node = document.createElement('li');
+
+
+// Step 2 
+
+const textNode = document.textNode('water')
+
+// step 3rd
+
+node.appendChild('textNode')
+
+//Step 4
+
+const add = document.querySelector('#myList').appendChild(node);
