385fd79b603253931a36d4c3ed817a58bdeffeeb
Ajay kumar <ajcoder25@gmail.com>
2024-04-08 06:53:11 +0530

    learned about the submit

--- Javascript-Procodrr/script.js
+++ Javascript-Procodrr/script.js
@@ -1,16 +1,40 @@
 const usernameInput = document.querySelector('#username');
+const para = document.querySelector('p')
+const form = document.querySelector('form')
 
-usernameInput.addEventListener('dblclick', () => {
-    console.log('input double clicked');
-});
+// usernameInput.addEventListener('dblclick', () => {
+//     console.log('input double clicked');
+// });
 
 
 
 
-//Input event 
+//Input event ---- whatever you write it will show on the console
 //You can put anything instead of e
 
-usernameInput.addEventListener('input',(e) =>{
-    console.log(e.target.value)
-    console.log('input event fired')
+//
+// usernameInput.addEventListener('input',(e) =>{
+//     console.log(e.target.value)
+//     para.innerText = e.target.value
+    
+// })
+
+
+//****************** */
+
+
+//Change event: it gets printed as come out of the input box
+// focus & blur : 
+// focus: when you click on input, it is called focus
+ //  blur : outside of the input box
+
+
+
+// usernameInput.addEventListener('focus', (e) =>{
+//     console.log(e)
+//     para.innerText = e.target.value
+// })
+
+form.addEventListener('click',(e)=>{
+    console.log('you have submit your form')
 })
