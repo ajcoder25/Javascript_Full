571fc69cc6a16cb9091815283b0059a8b8e18d0f
Ajay kumar <ajcoder25@gmail.com>
2024-04-27 07:05:36 +0530

    Local storage

--- first Javscript project/script.js
+++ first Javscript project/script.js
@@ -5,7 +5,18 @@ const progressBar = document.querySelector('.progress-bar')
 
 // will select the progress bar to make it work
 
-const progressValue = document.querySelector('progress-value')
+const progressValue = document.querySelector('.progress-value')
+
+
+
+// goal is to in local storage creating the object of the all goals
+
+// We can't create the goal object by hard coded so, we will create the empty object
+
+const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+
+
+
 
 
 
@@ -23,8 +34,11 @@ checkBoxList.forEach((checkbox) =>{
         })
 
         if(allGoalsAdded){
-            checkbox.parentElement.classList.toggle('Completed');
-            progressValue.style.width = '33.33%'
+            checkbox.parentElement.classList.toggle('Completed')
+            progressValue.style.width = '33.3%';
+            const inputId = checkbox.nextElementSibling.id
+            allGoals[inputId].completed = !allGoals[inputId].completed
+            localStorage.setItem('allGoals', JSON.stringify(allGoals));
             
         }
         else{
@@ -43,10 +57,29 @@ checkBoxList.forEach((checkbox) =>{
 
 //Will try to use the focus event when user start to write it should stop showing the error
 inputFields.forEach((input) =>{
+ input.value = allGoals[input.id].name
+
+ if(allGoals[input.id].completed){
+    input.parentElement.classList.add()
+ }
+
+ })
+
     input.addEventListener('focus', (e) =>{
         progressBar.classList.remove('show-error')
     })
-})
+
+    // As we enter the input in any of these it will print on the console
+    // you can check where event is occured using the console.log(e.target)
+    input.addEventListener('input', (e) =>{
+        allGoals[input.id] = {
+                       name: input.value,
+                       completed : false,
+        }
+    
+        localStorage.setItem('allGoals', JSON.stringify(allGoals))
+    })
+
 
 
 
